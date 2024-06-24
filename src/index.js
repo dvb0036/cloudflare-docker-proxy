fc4a9ae32aaae3fd399eafbaf393625bcf4dcaa1
dvb0036 <dubin.vip@gmail.com>
2024-06-24 17:13:23 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.luyami.top": dockerHub,
+  "quay.luyami.top": "https://quay.io",
+  "gcr.luyami.top": "https://gcr.io",
+  "k8s-gcr.luyami.top": "https://k8s.gcr.io",
+  "k8s.luyami.top": "https://registry.k8s.io",
+  "ghcr.luyami.top": "https://ghcr.io",
+  "cloudsmith.luyami.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.luyami.top": dockerHub,
 };
 
 function routeByHosts(host) {
